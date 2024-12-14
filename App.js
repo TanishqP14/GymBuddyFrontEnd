a4adc56282401e47121a1f4b729c34da6c1bdec3
tanishq <tanishqpunj2007@gmail.com>
2024-12-14 16:59:25 -0500

    welcome screen

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import WelcomeSceneFirst from "./WelcomeScene/WelcomeSceneFirst.js";
+import WelcomeSceneSec from "./WelcomeScene/WelcomeSceneSec.js";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <WelcomeSceneFirst></WelcomeSceneFirst>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen 1"
+        screenOptions={{headerShown: false, statusBarHidden: true}}>
+        <Stack.Screen
+          name="Screen 1"
+          component={WelcomeSceneFirst} />
+        <Stack.Screen 
+          name="Screen 2" 
+          component={WelcomeSceneSec} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
--- WelcomeScene/WelcomeSceneSec.js
+++ WelcomeScene/WelcomeSceneSec.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View, Image, Dimensions, Pressable} from 'react-native';
 import * as Font from 'expo-font';
 
-const headerImage = require("C:/Users/tanis/OneDrive/Desktop/ReactNative/GymBuddyFrontEnd/assets/Images/wsFirst.png");
+const headerImage = require("C:/Users/tanis/OneDrive/Desktop/ReactNative/GymBuddyFrontEnd/assets/Images/wsSecond.png");
 const {width, height} = Dimensions.get('window');
 
 
-export default function WelcomeSceneFirst({navigation}) {
+export default function WelcomeSceneSec({navigation}) {
 
   const [fontsLoaded] = Font.useFonts({
     'Lexend-Bold': require('C:/Users/tanis/OneDrive/Desktop/ReactNative/GymBuddyFrontEnd/assets/Fonts/Lexend/Lexend-Bold.ttf'),
@@ -16,23 +16,25 @@ export default function WelcomeSceneFirst({navigation}) {
     <View style={welcomeStyles1.container}>
       <Image source={headerImage} style={welcomeStyles1.image} />
       <View style={{margin: 20, marginTop: 50}}>
-        <Text style={[welcomeStyles1.textStyle, {fontWeight: 'bold', fontSize: 30, fontFamily: 'Lexend-Bold'}]}>Find the perfect workout 
-          partner or group</Text>
+        <Text style={[welcomeStyles1.textStyle, {fontWeight: 'bold', fontSize: 30, fontFamily: 'Lexend-Bold'}]}>Find a gym partner near you</Text>
       </View>
       <View style={{margin: 10, marginTop: 10}}>
-        <Text style={[welcomeStyles1.textStyle, {fontSize: 17, fontFamily: 'Lexend', color: 'lightgrey'}]}>Join a group workout session or find a 
-          personal trainer to get in shape. Meet new friends who share your fitness goals.</Text>
+        <Text style={[welcomeStyles1.textStyle, {fontSize: 17, fontFamily: 'Lexend', color: 'lightgrey'}]}>Get fit and make friends at the same time. </Text>
       </View>
       <View style={{flexDirection: "row", justifyContent: "space-evenly", width: width*0.3, height: 50, padding: 20, marginTop: 25}}>
-        <View style={[welcomeStyles1.dots, {backgroundColor: '#1A78E5'}]}></View>
         <View style={welcomeStyles1.dots}></View>
+        <View style={[welcomeStyles1.dots, {backgroundColor: '#1A78E5'}]}></View>
         <View style={welcomeStyles1.dots}></View>
       </View>
       <View>
-        <Pressable style={({pressed}) => [welcomeStyles1.startedButton, {backgroundColor: pressed ? '#334A66' : '#1A78E5'}]}
-          onPress={() => navigation.navigate("Screen 2")}>
+        <Pressable style={({pressed}) => [welcomeStyles1.startedButton, {backgroundColor: pressed ? '#334A66' : '#1A78E5'}]}>
           <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold', fontFamily: 'Lexend'}}>
-            Get Started
+            Sign Up with GT email
+          </Text>
+        </Pressable>
+        <Pressable style={({pressed}) => [welcomeStyles1.startedButton, {backgroundColor: pressed ? '#334A66' : '#486586', marginTop: 20}]}>
+          <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold', fontFamily: 'Lexend'}}>
+            Already have an account? Log IN
           </Text>
         </Pressable>
       </View>
